bd6383e83d3b2bf8525c74794e1ea5f41833e52f
buk55 <boltneffe@yandex.ru>
2021-01-13 17:35:17 +0500

    случайные окружности

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtGui import QColor,  QPainter
 from random import randint
 
@@ -9,7 +9,10 @@ from random import randint
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setGeometry(0, 0, 800, 600)
+        self.setWindowTitle('Разноцветные круги')
+        self.pushButton = QPushButton('Круг', self)
+        self.pushButton.move(0, 570)
         self.a = False
         self.pushButton.clicked.connect(self.run)
 
@@ -22,7 +25,7 @@ class MyWidget(QMainWindow):
         if self.a:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor('#ffff00'))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             a = randint(0, 300)
             qp.drawEllipse(400 - int(0.5 * a), 300 - int(0.5 * a), a, a)
 
